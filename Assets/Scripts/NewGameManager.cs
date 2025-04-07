5819d3afdae6b80fe9d9dbbcb843aec52f1cdd11
zinine73 <zinine@naver.com>
2025-04-07 11:50:46 +0900

    접속유저정보

--- Assets/Scripts/NewGameManager.cs
+++ Assets/Scripts/NewGameManager.cs
@@ -1,11 +1,23 @@
 using Photon.Pun;
 using UnityEngine;
 using Photon.Realtime;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class NewGameManager : MonoBehaviourPunCallbacks
 {
+    public TMP_Text roomName;
+    public TMP_Text connectInfo;
+    public TMP_Text msgList;
+    public Button exitBtn;
+
     void Awake()
     {
         CreatePlayer();
+        // 접속 정보 추출 및 표시
+        SetRoomInfo();
+        // exit 버튼 이벤트 연결
+        exitBtn.onClick.AddListener(() => OnExitClick());
     }
 
     void CreatePlayer()
@@ -19,5 +31,40 @@ public class NewGameManager : MonoBehaviourPunCallbacks
                             points[idx].position,
                             points[idx].rotation,
                             0);
+    }
+
+    void SetRoomInfo()
+    {
+        Room room = PhotonNetwork.CurrentRoom;
+        roomName.text = room.Name;
+        connectInfo.text = $"({room.PlayerCount}/{room.MaxPlayers})";
+    }
+
+    // exit 버튼 연결 함수
+    private void OnExitClick()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+
+    // 포톤 룸에서 퇴장했을 때 호출되는 콜백 함수
+    public override void OnLeftRoom()
+    {
+        SceneManager.LoadScene("Lobby");
+    }
+
+    // 룸으로 새로운 네트워크 유저가 접속했을 때 호출되는 콜백함수
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        SetRoomInfo();
+        string msg = $"\n<color=#00ff00>{newPlayer.NickName}</color> is joined room";
+        msgList.text += msg;
+    }
+
+    // 룸에서 네트워크 유저가 퇴장 했을 때 호출되는 콜백 함수
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        SetRoomInfo();
+        string msg = $"\n<color=#ff0000>{otherPlayer.NickName}</color> is left room";
+        msgList.text += msg;
     }
 }
